47b4be8ed8af9983aad9e4ccadf8a9e76cc8b973
MartSchkiperov <32428093+MartSchkiperov@users.noreply.github.com>
2017-10-15 20:57:05 +0300

    getting and printing

--- main/java/HttpUtility.java
+++ main/java/HttpUtility.java
@@ -1,9 +1,23 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class HttpUtility {
 
-    public static HttpURLConnection makeHttpGetRequest(String url) {
-        return null;
+
+    public static String makeHttpGetRequest(String urlToGet) throws Exception {
+        StringBuilder result = new StringBuilder();
+        URL url = new URL(urlToGet);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        BufferedReader read = new BufferedReader(new InputStreamReader(con.getInputStream()));
+        String line;
+        while ((line = read.readLine()) != null) {
+            result.append(line);
+        }
+        read.close();
+        return result.toString();
     }
 
 
--- main/java/WeatherRequest.java
+++ main/java/WeatherRequest.java
@@ -1,6 +1,15 @@
 public class WeatherRequest {
 
-    String cityName;
+    static String cityName = "Tallinn";
+    static String weatherString = "";
+    static String apiCode = "ed2d0441550bad29e87a82b03e7ff532";
+
+
+    public static void main(String[] args) throws Exception {
+        weatherString = HttpUtility.makeHttpGetRequest("http://api.openweathermap.org/data/2.5/forecast?q="
+                + cityName + "&appid=" + apiCode);
+        System.out.println(weatherString);
+    }
 
     public WeatherRequest() {
 
@@ -14,7 +23,5 @@ public class WeatherRequest {
     public String getCountryName() {
         return "none";
     }
-
-
 
 }
